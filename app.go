c4b971e63b20e9378b9d4a17ceb21b9f3af38dcb
Pranav Rk <pranavrk7@gmail.com>
2021-10-23 23:00:47 +0530

    Web dev basics

--- app.go
+++ app.go
@@ -6,7 +6,14 @@ import (
 )
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!!")
+	fmt.Fprintf(w, "<h1>Welcome!!</h1>")
+    /* Multi-line tags and tags will be in the same format in the page source
+	fmt.Fprintf(w,
+		`<h1>Welcome!!</h1>
+        <p>This is the first sample</p>
+        <h2>Visit us again!!</h2>
+    `)
+    */
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request) {
